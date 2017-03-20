c3eacf5b235d66fb3f0b69343261712404efec7a
KAU <KAU@KAU-PC>
2017-03-20 17:59:26 +0900

    CheckingTest

--- 2주차실습/src/계좌/Account.java
+++ 2주차실습/src/계좌/Account.java
@@ -30,6 +30,8 @@ public class Account {
    {
       return balance;
    }
+   public void setBalance()
+   {
 
 }
 
---
+++ 2주차실습/src/계좌/CheckingAccount.java
@@ -0,0 +1,22 @@
+package °èÁÂ;
+
+public class CheckingAccount extends Account {
+	
+	private double credit_limit;
+	private double interest;
+	private double loan_interest;
+	
+	public double debit(double a){
+		return credit_limit;
+	}
+	public double nextMonth(double b){
+		if(credit_limit>0){
+			interest=credit_limit+credit_limit*0.5;
+			System.out.println(interest);
+		}
+		else{
+			
+		}
+			
+		}
+	}
